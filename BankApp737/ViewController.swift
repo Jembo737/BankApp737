b8752180c598dc532b20a836aba5e2093877c2e9
Jemal <punk16.66@mail.ru>
2020-11-24 18:12:54 +0300

    Added loan amount calculation

--- BankApp737/ViewController.swift
+++ BankApp737/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 class ViewController: UIViewController {
    
     // MARK: - Outlets
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
     
     @IBOutlet weak var amountLabel: UILabel?
+    @IBOutlet weak var monthlyPaymentLabel: UILabel?
     @IBOutlet weak var pvTextField: UITextField?
     @IBOutlet weak var nperTextField: UITextField?
     @IBOutlet weak var rateTextField: UITextField?
@@ -33,16 +38,29 @@ class ViewController: UIViewController {
         amountLabel?.text = formatter.string(from: number)
     }
     
+    func updateMonthlyAmountLabel() {
+        monthlyPaymentLabel?.text = nil
+        
+        guard let nper = Double(nperTextField?.text ?? "") else { return }
+        guard let pv = Double(pvTextField?.text ?? "") else { return }
+        guard let rate = Double(rateTextField?.text ?? "") else { return }
+        
+        let monthlyPayment = abs(ExcelFormulas.pmt(rate: rate / 100 / 12, nper: nper, pv: pv))
+        monthlyPaymentLabel?.text = "\(monthlyPayment)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updateAmountLabel()
-        
+        updateMonthlyAmountLabel()
     }
 
+    // MARK: - Actions
     @IBAction func textFieldEditingChanged(_ sender: UITextField) {
-        print(sender.text ?? "nil")
+        updateMonthlyAmountLabel()
     }
-    
 }
+
+
 
